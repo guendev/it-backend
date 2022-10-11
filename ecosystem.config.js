4932ee84affd9f49abb02782f54c158cd2002c07
Yuan <nguyenshort@gmail.com>
2022-10-11 15:23:37 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'it-square',
+      name: 'it-geto',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: 'npm',
-      args: 'run preview'
+      args: 'run start'
     }
   ]
 }
