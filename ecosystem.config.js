444c23072847bbfc30dab3b8c98d69202853292c
yuan <nguyenshort@gmail.com>
2022-11-16 18:33:10 +0700

    add remove

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'it-geto',
+      name: 'it-backend',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: 'npm',
