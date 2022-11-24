3e572650a34a2fa8144520467fdb504409c81b74
yuan <nguyenshort@gmail.com>
2022-11-24 16:23:13 +0700

    add comment level

--- src/app/posts/posts.resolver.ts
+++ src/app/posts/posts.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
 import { PostsService } from './posts.service'
 import { Post } from './entities/post.entity'
 import { CreatePostInput } from './dto/create-post.input'
