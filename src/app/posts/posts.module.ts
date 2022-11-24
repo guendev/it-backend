52afc40e3442c3e104ff8a361dbba3050590212d
yuan <nguyenshort@gmail.com>
2022-11-24 15:36:10 +0700

    add comment level

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { PostsResolver } from './posts.resolver';
+import { Module } from '@nestjs/common'
+import { PostsService } from './posts.service'
+import { PostsResolver } from './posts.resolver'
+import { MongooseModule } from '@nestjs/mongoose'
+import { PostEntity } from '@app/posts/entities/post.entity'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: PostsModule.name,
+        useFactory: () => {
+          return PostEntity
+        }
+      }
+    ])
+  ],
   providers: [PostsResolver, PostsService]
 })
 export class PostsModule {}
