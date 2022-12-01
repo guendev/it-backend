f27e0d5867e541bc5993c656f18879bf74b0a757
yuan <nguyenshort@gmail.com>
2022-12-01 16:24:46 +0700

    add comment level

--- src/app/bookmarks/bookmarks.resolver.ts
+++ src/app/bookmarks/bookmarks.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
 import { BookmarksService } from './bookmarks.service'
 import { Bookmark } from './entities/bookmark.entity'
 import { CreateBookmarkInput } from './dto/create-bookmark.input'
-import { UseGuards } from '@nestjs/common'
+import { Inject, UseGuards } from '@nestjs/common'
 import { JWTAuthGuard } from '@guards/jwt.guard'
 import { CurrentUser } from '@decorators/user.decorator'
 import { User, UserDocument } from '@app/users/entities/user.entity'
@@ -12,12 +12,16 @@ import { NotFoundError } from '@shared/errors/not-found.error'
 import { InputValidator } from '@shared/validator/input.validator'
 import { GetBookmarkInput } from '@app/bookmarks/filters/get-bookmark.input'
 import { GetBookmarksInput } from '@app/bookmarks/filters/get-bookmarks.input'
+import { PUB_SUB } from '@apollo/pubsub.module'
+import { RedisPubSub } from 'graphql-redis-subscriptions'
+import ChanelEnum from '@apollo/chanel.enum'
 
 @Resolver(() => Bookmark)
 export class BookmarksResolver {
   constructor(
     private readonly bookmarksService: BookmarksService,
-    private readonly projectsService: ProjectsService
+    private readonly projectsService: ProjectsService,
+    @Inject(PUB_SUB) private pubSub: RedisPubSub
   ) {}
 
   @Mutation(() => Bookmark, { nullable: true })
@@ -39,11 +43,18 @@ export class BookmarksResolver {
     })
 
     if (_record) {
+      await this.pubSub.publish(ChanelEnum.NOTIFY, {
+        subNotify: { user, msg: 'Theo dõi thành cồn' }
+      })
       return this.bookmarksService.remove({
         project: _project._id,
         user: new Types.ObjectId(user.id)
       })
     }
+
+    await this.pubSub.publish(ChanelEnum.NOTIFY, {
+      subNotify: { user, msg: 'Huỷ theo dõi thành công' }
+    })
 
     return this.bookmarksService.create(user as UserDocument, _project)
   }
